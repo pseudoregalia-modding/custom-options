3a503a5a0f56169e8a82a5962af86b2e32ea6d08
turncoda <144861042+turncoda@users.noreply.github.com>
2024-05-14 21:35:51 -0700

    Update make_pak.py to write directly to game pak folder

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 set game_folder=C:\Program Files (x86)\Steam\steamapps\common\Pseudoregalia\pseudoregalia
 
-make_pak.py --asset-list asset_list.rsp
-copy asset_list_p.pak "%game_folder%\Content\Paks\custom_options_framework_p.pak"
+make_pak.py --asset-list asset_list.txt --output "%game_folder%\Content\Paks\custom_options_framework_p.pak"
 REM call "%game_folder%\Binaries\Win64\pseudoregalia-Win64-Shipping.exe"
 pause
