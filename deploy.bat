594e0c2d6c56e6e7424d99fec89599f8ed7fb305
turncoda <144861042+turncoda@users.noreply.github.com>
2026-08-28 15:22:06 -0400

    Add more convenience build scripts

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,3 @@
 set game_folder=C:\Program Files (x86)\Steam\steamapps\common\Pseudoregalia\pseudoregalia
 
 make_pak.py --asset-list asset_list.txt --output "%game_folder%\Content\Paks\custom_options_framework_p.pak"
-REM call "%game_folder%\Binaries\Win64\pseudoregalia-Win64-Shipping.exe"
-pause
